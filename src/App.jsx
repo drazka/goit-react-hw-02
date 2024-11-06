9de35e42a4352563eebd9b8d0f37988fefd7edd9
drazka <izabdr@gmail.com>
2024-11-06 21:54:45 +0100

    step3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Description from './components/description/Description'
 import Options from './components/options/Options'
 import Feedback from './components/feedback/Feedback'
+import Notification from './components/notification/Notification';
 
 const App = () => {
   const [feedback, setFeedback] = useState({
@@ -20,11 +21,18 @@ const App = () => {
     }));
   };
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback > 0 ? (((feedback.good + (feedback.neutral / 2)) / totalFeedback) * 100).toFixed(2) : 0;
+
   return (
     <>
       <Description />
       <Options updateFeedback={updateFeedback} />
-      <Feedback feedback={feedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} total={totalFeedback} positivePercentage={positivePercentage} />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
     </>
   );
 };
--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,19 +1,19 @@
 import clsx from "clsx";
 import css from "./Feedback.module.css";
 
-const Feedback = ({ feedback }) => {
-    const total = feedback.good + feedback.neutral + feedback.bad;
-    const positivePercentage = total > 0 ? (((feedback.good + (feedback.neutral / 2)) / total) * 100).toFixed(2) : 0;
+const Feedback = ({ feedback, total, positivePercentage }) => {
+
   
-    return (
-      <div className={css.feedback}>
-        <p>Good: {feedback.good}</p>
-        <p>Neutral: {feedback.neutral}</p>
-        <p>Bad: {feedback.bad}</p>
-        <p>Total: {total}</p>
-        <p>Positive: {positivePercentage}%</p>
-      </div>
-    );
-  };
-  
-  export default Feedback;
+
+  return (
+    <div className={css.feedback}>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive: {positivePercentage}%</p>
+    </div>
+  );
+};
+
+export default Feedback;
